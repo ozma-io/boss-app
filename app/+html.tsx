a1d8cd75fb2e93fbbb5a362965ac4881ff3c8f40
Kirill Markin <markinkirill@gmail.com>
2025-11-16 15:21:51 +0100

    refactor: remove dark mode background styling from HTML
    - Eliminated the dark mode background color styling from the main HTML file.
    - This change simplifies the CSS and focuses on a single background color for the application.

--- app/+html.tsx
+++ app/+html.tsx
@@ -36,11 +36,6 @@ const responsiveBackground = `
 body {
   background-color: #fff;
 }
-@media (prefers-color-scheme: dark) {
-  body {
-    background-color: #000;
-  }
-}
 * {
   outline: none !important;
 }
