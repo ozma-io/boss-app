ac23d2afc2af014361c1f2dce2a9605e853b90a8
Kirill Markin <markinkirill@gmail.com>
2025-11-22 10:08:14 +0100

    Bump version to 1.3.1

--- app.config.ts
+++ app.config.ts
@@ -29,7 +29,7 @@ const config: ExpoConfig = {
   name: 'BossUp',
   slug: 'boss-app',
   owner: 'ozma-io',
-  version: '1.3.0',
+  version: '1.3.1',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'bossup',
